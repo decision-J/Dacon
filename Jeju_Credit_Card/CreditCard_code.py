248350460be367257897d97d63513fe1c2e51e82
JoungHaeyong <60501448+JoungHaeyong@users.noreply.github.com>
2020-07-17 16:21:48 +0900

    Update CreditCard_code.py

--- Jeju_Credit_Card/CreditCard_code.py
+++ Jeju_Credit_Card/CreditCard_code.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import warnings
+
+import seaborn as sns
 warnings.filterwarnings(action='ignore')
 
 os.getcwd()
@@ -13,3 +15,44 @@ dataset.info()
 dataset.head()
 dataset.isnull().sum()
 print(dataset[dataset.isnull().any(axis=1)].head())
+
+submission = pd.read_csv('submission.csv')
+submission.isnull().sum()
+submission.info()
+submission.head()
+submission.CARD_SIDO_NM.value_counts()
+submission.STD_CLSS_NM.value_counts()
+set(dataset.STD_CLSS_NM)
+
+len(set(dataset.STD_CLSS_NM))
+len(submission.STD_CLSS_NM.value_counts())
+
+len(set(dataset.CARD_SIDO_NM))
+len(submission.CARD_SIDO_NM.value_counts())
+
+import matplotlib.pyplot as plt
+max(dataset.AMT/10000)
+
+sns.distplot(dataset.AMT/10000)
+plt.title("AMT distplot")
+plt.show()
+
+sns.distplot(dataset[dataset.AMT/10000 < 10000].AMT/10000)
+plt.title("AMT distplot")
+plt.show()
+
+sns.distplot(dataset[dataset.AMT/10000 >= 10000].AMT/10000)
+plt.title("AMT distplot")
+plt.show()
+
+sns.boxplot(x = "AMT",  data = dataset)
+plt.show()
+
+len(dataset[dataset.AMT/10000 < 1000])
+len(dataset[dataset.AMT/10000 < 2000]) - len(dataset[dataset.AMT/10000 < 1000])
+len(dataset[dataset.AMT/10000 < 10000]) - len(dataset[dataset.AMT/10000 < 2000])
+len(dataset[dataset.AMT/10000 >= 10000])
+
+
+## Baseline : RF regressor
+from sklearn.ensemble import RandomForestRegressor
